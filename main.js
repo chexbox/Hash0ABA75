98cb144011a8bc579142cbb6ad7bd6503282130a
chexbox <31783961+chexbox@users.noreply.github.com>
2018-06-29 17:48:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //var theme { "name": "Example Page", "nav": [["first", "firstlink"], ["second","secondlink"]]};
 function setcookie(value) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (90*24*60*60*1000));
     var expires = "expires="+ d.toUTCString()
     document.cookie = "";
     document.cookie = "colors=" + value + ";" + expires + " path=/";
